566695ba9124d2ba3d36385a95273b64bcd0e5dd
Fadhlan Hadaina <freehatin55@gmail.com>
2020-05-23 00:15:18 +0700

    Nambah gambar, ganti konsep

--- js/response.js
+++ js/response.js
@@ -1,40 +1,34 @@
 
+var url_ = "http://localhost/hitung-bangun-datar/api/hitungbangundatar"
 function fill(id) {
     var x = document.getElementById(id).value;
     if(x == "" || x == null || x == 0)
         document.getElementById(id).value = 1;
 }
 function luasPersegi() {
-    var sisi = document.getElementById("sisiPersegi").value;
-    if(sisi != null && sisi != "") {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if(this.readyState == 4 && this.status == 200) {
+    var sisi = document.getElementById('sisiPersegi').value;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if(this.status == 200) {
                 var r = JSON.parse(this.responseText);
-                document.getElementById("answerPersegi").innerHTML = "Luas persegi = "+r.hasil+" cm²";
+                document.getElementById("answer").innerHTML = "Luas persegi = "+r.hasil+" cm²";
+            }
+            else if(this.status == 400) {
+                document.getElementById("answer").innerHTML = "Tidak boleh kosong / 0";
             }
         }
-        xhttp.open("GET", "http://localhost/hitung-bangun-datar/api/hitungbangundatar/luas_persegi?sisi="+sisi);
-        xhttp.send();
-    }
-    else {
-        document.getElementById("answerPersegi").innerHTML = "Tidak boleh kosong!";
-    }
+    };
+    xhttp.open("GET", url_+"/luas_persegi?sisi="+sisi, true);
+    xhttp.send();
 }
-function kelilingPersegi() {
-    var sisi = document.getElementById("sisiPersegi").value;
-    if(sisi != null && sisi != "") {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if(this.readyState == 4 && this.status == 200) {
-                var r = JSON.parse(this.responseText);
-                document.getElementById("answerPersegi").innerHTML = "Keliling persegi = "+r.hasil+" cm";
-            }
-        }
-        xhttp.open("GET", "http://localhost/hitung-bangun-datar/api/hitungbangundatar/keliling_persegi?sisi="+sisi);
-        xhttp.send();
-    }
-    else {
-        document.getElementById("answerPersegi").innerHTML = "Tidak boleh kosong!";
-    }
+function show(id) {
+    document.getElementById(id).style.display = "block";
+}
+function hide(id) {
+    document.getElementById(id).style.display = "none";
+}
+function mulaiHitung() {
+    document.getElementById('welcome').style.display = "none";
+    document.getElementById('home').style.display = "block";
 }
